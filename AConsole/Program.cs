f43842752b86267074318a24e743ec8dac7ec556
Dynesshely <catrol@qq.com>
2022-12-10 03:13:33 +0800

    📦 Struct: 移动目录结构

---
+++ AConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.GUIDebugConsole.AConsole;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        
+        
+        return 0;
+    }
+}
